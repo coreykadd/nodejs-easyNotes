3a18c59f83eae72108334caa399b027f7e0fd23d
coreykadd <coreymangan22@live.ie>
2018-02-07 12:23:26 +0000

    setup controller.js

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -1,1 +1,28 @@
 //note.controller.js
+
+var note = require('../models/note.model.js');
+
+//Create and save new note
+exports.create = function(req, res){
+
+};
+
+//Retrive and return all notes
+exports.findAll = function(req, res){
+
+};
+
+//Retrieve and return one note
+exports.findOne = function(req, res){
+
+};
+
+//update a note
+exports.update = function(req, res){
+
+};
+
+//Delte a note
+exports.delete = function(req, res){
+
+};
