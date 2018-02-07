c5a8ba161d1863023feaaa1f5d66797a8ed8ef5e
coreykadd <coreymangan22@live.ie>
2018-02-07 12:17:41 +0000

    setup routes

---
+++ app/routes/note.routes.js
@@ -0,0 +1,20 @@
+//note.routes.js
+
+module.exports = function(app) {
+    var notes = require('../controllers/note.controller.js');
+
+    //Create new note
+    app.post('/notes', notes.create);
+
+    //Retrieve all notes
+    app.get('/notes', notes.findAll);
+
+    //Retrieve a single note
+    app.get('notes:noteId', notes.findOne);
+
+    //Update a note
+    app.put('notes/:noteId', notes.update);
+
+    //Delete a note
+    app.delete('notes/:noteId', notes.delete);  
+}
