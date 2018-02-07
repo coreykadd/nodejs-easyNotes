30045f6e32ecf4d63fab97c4a5cc66f14f986bf9
coreykadd <coreymangan22@live.ie>
2018-02-07 14:07:14 +0000

    fixes

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -1,13 +1,13 @@
 //note.controller.js
 
-var note = require('../models/note.model.js');
+var Note = require('../models/note.model.js');
 
 //Create and save new note
 exports.create = function(req, res){
-    if(!req.body.content)
+    if(req.body.content = '')
         res.status(400).send({message: "Note cannot be empty"});
     
-    var note = new note({
+    var note = new Note({
         title: req.body.title || "Untitled Note",
         content: req.body.content
     });
@@ -26,7 +26,7 @@ exports.create = function(req, res){
 
 //Retrive and return all notes
 exports.findAll = function(req, res){
-    note.find(function(err, data){
+    Note.find(function(err, data){
         if(err)
             res.status(500).send({message: "Some error occured while retrieving notes"});
         else
@@ -36,7 +36,7 @@ exports.findAll = function(req, res){
 
 //Retrieve and return one note
 exports.findOne = function(req, res){
-    note.findById(req.params.noteId, function(err, data){
+    Note.findById(req.params.noteId, function(err, data){
         if(err)
             res.status(500).send({message: "Could not retrive note with id " + req.params.noteId});
         else
@@ -46,7 +46,7 @@ exports.findOne = function(req, res){
 
 //update a note
 exports.update = function(req, res){
-    note.findById(req.params.noteId, function(err, data){
+    Note.findById(req.params.noteId, function(err, data){
         if(err)
             res.status(500).send({message: "Could not find note with id " + req.params.noteId});
         
@@ -64,7 +64,7 @@ exports.update = function(req, res){
 
 //Delte a note
 exports.delete = function(req, res){
-    note.remove({_id: req.params.noteId}, function(err, data){
+    Note.remove({_id: req.params.noteId}, function(err, data){
         if(err)
             res.status(500).send({message: "Could not delete note with id " + req.params.noteId});
         else
